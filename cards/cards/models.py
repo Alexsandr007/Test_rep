23daecda50c49249ca22c3243e02a9f61f6af9c6
Alexandr007 <aleksone_01@mail.ru>
2023-02-13 09:30:02 +0300

    Changed settings to update directory visibility and created db

---
+++ cards/cards/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+
+
+class DiscountPercent(models.Model):
+    name = models.CharField(max_length=25)
+    percent = models.FloatField()
+
+
+class Orders(models.Model):
+    number = models.IntegerField(unique=True)
+    date = models.DateTimeField(auto_now_add=True)
+    sum = models.IntegerField()
+    percent = models.IntegerField()
+    discount_amount = models.IntegerField()
+
+
+class Goods(models.Model):
+    order = models.ManyToManyField(Orders)
+    name = models.CharField(max_length=50)
+    cost = models.FloatField()
+    discount_cost = models.FloatField()
+
+
+class Status(models.TextChoices):
+    active = 'Active'
+    inactive = 'Inactive'
+    frozen = 'Frozen'
+
+
+class Card(models.Model):
+    series = models.IntegerField()
+    number = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    date_end = models.DateTimeField()
+    latest_use = models.DateTimeField(auto_now=True)
+    summa_purchases = models.IntegerField()
+    status = models.CharField(
+        max_length=25,
+        choices=Status.choices,
+        default=Status.active
+    )
+    percent = models.ManyToManyField(DiscountPercent)
+    order = models.ManyToManyField(Orders)
+
