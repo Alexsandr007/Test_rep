27f022f35ef666007c318d8e5c36ad4e59d9e76a
Alex_djangp_projects <aleksone_01@mail.ru>
2023-03-17 12:27:07 +0300

    change

--- cards/cards/views.py
+++ cards/cards/views.py
@@ -65,7 +65,7 @@ class CardViewSet(ModelViewSet):
                     except:
                         goods = False
                     if goods:
-                        print(lol)
+                        print(lollolo)
                         order.goods.add(goods)
                         goods.orders_set.add(order)
                         order.save()
